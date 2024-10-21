1ae85b5654b4aa41974846a181c43e8fba1b86dc
Jan Porębski <120678651+Janek003@users.noreply.github.com>
2024-10-21 20:45:53 +0200

    zadanie_3_8

---
+++ zadanie_3_8/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float x=0, a = 0, b = 0, c = 0, d = 0;
+    printf("input x: ");
+    scanf("%f", &x);
+
+    a = 10 * cos(x) - 0.1 * x * x + sin(x) + sqrt(4 * x * x + 7);
+    b = log(x + 5) + exp(x+1) - abs(tan(x)+1);
+    c = ( pow( sin(x), 2 ) + 0.5 )/(cos(pow(x,4)) + pow(tan(x*x), 4) );
+    d = sqrt( abs( 5 * sin( pow( x, 5 ) + 1 ) )/( 3.5 * pow( sin(x) + cos(x), 2 ) ) );
+
+    printf("(a): %f\n(b): %f\n(c): %f\n(d): %f", a, b, c, d);
+
+    return 0;
+}
